37e60259400a7b7c71616500123ed14bb7bb8b46
umeh-promise <umehpromise1000@gmail.com>
2026-04-24 00:20:32 +0100

    fix update AWS_DEFAULT_REGION format in workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <div>
-      <h1>Hi, there haha</h1>
+      <h1>Hi, dear</h1>
       <hr />
       <MarketingApp />
     </div>
